7c370292683b16136f0e382977a496ac7f0281eb
Ronimeister <ronimeisterhs@gmail.com>
2018-07-22 19:22:07 +0300

    Binary search added

---
+++ BinarySearchLib/BinarySearch.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchLib
+{
+    /// <summary>
+    /// Class that provides functionality for binary search
+    /// </summary>
+    /// <typeparam name="T">Expected type</typeparam>
+    public static class BinarySearch<T>
+    {
+        #region Public API
+        /// <summary>
+        /// Method that returns position of <paramref name="element"/> in <paramref name="array"/> if such is exist
+        /// </summary>
+        /// <param name="array">Needed array</param>
+        /// <param name="element">Needed element to find</param>
+        /// <exception cref="ArgumentNullException">Throws when:
+        /// 1) <paramref name="array"/> is equal to null
+        /// 2) <paramref name="element"/> is equal to null
+        /// </exception>
+        /// <returns>position of <paramref name="element"/> in <paramref name="array"/> if such is exist</returns>
+        public static int Search(T[] array, T element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException($"{nameof(element)} can't be equal to null!");
+            }
+
+            if (array == null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can't be equal to null!");
+            }
+
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+            
+            return Find(array, element, null);
+        }
+
+        /// <summary>
+        /// Method that returns position of <paramref name="element"/> in <paramref name="array"/> if such is exist based on <paramref name="comparer"/>
+        /// </summary>
+        /// <param name="array">Needed array</param>
+        /// <param name="element">Needed element to find</param>
+        /// <param name="comparer"><see cref="IComparer{T}"/> comparer</param>
+        /// <exception cref="ArgumentNullException">Throws when:
+        /// 1) <paramref name="array"/> is equal to null
+        /// 2) <paramref name="element"/> is equal to null
+        /// 3) <paramref name="comparer"/> is equal to null
+        /// </exception>
+        /// <returns>position of <paramref name="element"/> in <paramref name="array"/> if such is exist based on <paramref name="comparer"/></returns>
+        public static int Search(T[] array, T element, IComparer<T> comparer)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException($"{nameof(element)} can't be equal to null!");
+            }
+
+            if (array == null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can't be equal to null!");
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException($"{nameof(comparer)} can't be equal to null!");
+            }
+
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            return Find(array, element, comparer);
+        }
+        #endregion
+
+        #region Private methods
+        private static int Find(T[] array, T element, IComparer<T> comparer)
+        {
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+            }
+
+            int lowerBound = array.GetLowerBound(0);
+            int higherBound = lowerBound + array.Length - 1;
+
+            while (lowerBound <= higherBound)
+            {
+                int i = lowerBound + ((higherBound - lowerBound) >> 1);
+                int order = comparer.Compare(array[i], element);
+
+                if (order == 0)
+                {
+                    return i;
+                }
+
+                if (order < 0)
+                {
+                    lowerBound = i + 1;
+                }
+                else
+                {
+                    higherBound = i - 1;
+                }
+            }
+
+            return -1;
+        }
+        #endregion
+    }
+}
---
+++ BinarySearchLibTests/BinarySearchTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using BinarySearchLib;
+using NUnit.Framework;
+
+namespace BinarySearchLibTests
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        [TestCase(new double[] { 1, 2, 3, 4 }, 3, ExpectedResult = 2)]
+        [TestCase(new double[] { 1, 2, 3, 4, 5, 6, 7 }, 1, ExpectedResult = 0)]
+        [TestCase(new double[] { 1, 2, 3, 4, 5, 6, 7 }, 7, ExpectedResult = 6)]
+        [TestCase(new double[] { 1, 2, 3, 4, 5, 6, 7, 7 }, 7, ExpectedResult = 6)]
+        [TestCase(new double[] { 1, 2, 3, 4 }, 6, ExpectedResult = -1)]
+        public int BinarySearch_IsCorrect(double[] array, double element)
+            => BinarySearch<double>.Search(array, element);
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 3, ExpectedResult = 2)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 1, ExpectedResult = 0)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 7, ExpectedResult = 6)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 7 }, 7, ExpectedResult = 6)]
+        [TestCase(new int[] { 1, 2, 3, 4 }, 6, ExpectedResult = -1)]
+        public int BinarySearch_WithComparer_IsCorrect(int[] array, int element)
+        {
+            int comparisson(int a, int b)
+            {
+                if (a > b)
+                {
+                    return 1;
+                }
+
+                if (a < b)
+                {
+                    return -1;
+                }
+
+                return 0;
+            }
+
+            Comparer<int> comparer = Comparer<int>.Create(comparisson);
+
+            return BinarySearch<int>.Search(array, element, comparer);
+        }
+
+        [Test]
+        public void BinarySearch_NullArray_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => BinarySearch<object>.Search(null, 1));
+
+        [Test]
+        public void BinarySearch_NullElemnt_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => BinarySearch<object>.Search(new object[] { 1, 2, 3 }, null));
+
+        [Test]
+        public void BinarySearch_NullComparer_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => BinarySearch<object>.Search(new object[] { 1, 2, 3}, 1, null));
+    }
+}
